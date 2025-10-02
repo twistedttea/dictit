1114b1bb3652b3056d394c74e0846ff8ccc4d2cc
twistedttea <twistedtteavr@gmail.com>
2025-10-02 23:45:07 +0000

    feat: hello world app
    Add helloworld app
    Add install instructions
    Modify urls to include webapp

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from helloworldapp.views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
+
 ]
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # setup
+mkdir env_webproject
+python -m venv env_webproject
+source env_webproject/bin/activate
 pip install -r requirements.txt
