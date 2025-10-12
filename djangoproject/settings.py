1ac61bec855537c32cc780c6700e556f0f5df5e7
twistedttea <twistedtteavr@gmail.com>
2025-10-11 20:28:18 -0500

    featmove to postgresql

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -73,11 +73,21 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "USERS",
+        "USER": "django_admin",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "8000",
     }
 }
+
+
 
 
 # Password validation
