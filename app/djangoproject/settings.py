9d10baa72a2685e63e24133a3225491c0c11570b
twistedttea <twistedtteavr@gmail.com>
2025-10-17 01:34:28 -0500

    feat: paste object
    create paste object that stores date, title, and content, bound to a
    UUID
    Need to fix malformed UUID's when requesting them with 'obj'.id

--- app/djangoproject/settings.py
+++ app/djangoproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "wait_for_db",
+    "pastes.apps.PastesConfig",
 ]
 
 MIDDLEWARE = [
@@ -73,18 +76,18 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "USERS",
-        "USER": "django_admin",
-        "PASSWORD": "password",
-        "HOST": "localhost",
-        "PORT": "8000",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": os.getenv("DB_NAME"),  # Name of your MySQL database
+    #     "USER": os.getenv("DB_USER"),  # MySQL username
+    #     "PASSWORD": os.getenv("DB_PASSWORD"),  # MySQL password
+    #     "HOST": os.getenv("DB_HOST"),  # MySQL service name
+    #     "PORT": "3306",  # MySQL default port
+    # }
 }
 
 
@@ -125,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/app/staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- app/djangoproject/urls.py
+++ app/djangoproject/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from helloworldapp.views import hello
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello),
+    path('pastes/', include('pastes.urls'))
 
 ]
+ 
