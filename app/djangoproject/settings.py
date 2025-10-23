98780b9b19448e00c29abdaeeb4c4d98e8ca4ed9
twistedttea <twistedtteavr@gmail.com>
2025-10-22 20:53:55 -0500

    feat: css & styling

--- app/djangoproject/settings.py
+++ app/djangoproject/settings.py
@@ -128,7 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/app/staticfiles"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "staticfiles",
+# ]
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- app/pastes/forms.py
+++ app/pastes/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django import forms
 
 from .models import pastebin_entry
 
@@ -8,7 +7,19 @@ class paste_form(forms.ModelForm):
     class Meta:
         model = pastebin_entry
         fields = ["title", "content"]
-        contentwidget = {"content": forms.Textarea(attrs={"rows": 25, "cols": 80})}
+        widget = {
+            "content": forms.Textarea(attrs={
+                "class": "form-text-field",
+                "placeholder": "Enter text here.",
+                "rows": 25,
+                "cols": 80
+            }),
+            # "title": forms.CharField(attrs={
+            #     "class": "form-control",
+            #     "placeholder": "Enter paste title"
+            # }),
+
+        }
 
     # def sparkly_clean(self):
     #     content = self.cleaned_data["content"]
